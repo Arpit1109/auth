a5520b569c53bff9e45e78a104a40d4b51895350
Arpit <arpitagrahari19@gmail.com>
2022-10-12 13:56:28 +0530

    issues detected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Form } from 'react-bootstrap';
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         
+      <div className="a">
+     
+        <div className='c'>
+        <h1 className='heading '>Sign in</h1>
+          <Form className='d'>
+          <Form.Group className="mb-3 deco " controlId="formBasicName" >
+          
+              <Form.Control type="text" className='sinput' placeholder="Your name"  />
+
+            </Form.Group>
+            <Form.Group className="mb-3 deco " controlId="formBasicEmail">
+              
+              <Form.Control type="email" className='sinput' placeholder="Your email" />
+
+            </Form.Group>
+           
+
+            <Form.Group className="mb-3 deco" controlId="formBasicPassword">
+             
+              <Form.Control type="password" className='sinput' placeholder="Your password" />
+            </Form.Group>
+            <Form.Group className="mb-3 " controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="I agree all statements in Terms and Services" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+
+        </div>
+        <div className='i'> 
+        <img src='signup-image.jpg' alt='sign' />
+        </div>
+      </div>
+
     </div>
   );
 }
